948cba5126501665b04009b2cd066cd1a1edd63f
Sepren <LexusLFA477@gmail.com>
2022-02-13 01:17:16 +0200

    Modern the second task

--- second_task.php
+++ second_task.php
@@ -4,5 +4,11 @@
     echo $number;
     $new_number = 10;
     echo $new_number, "\n";
+    $float_number = 1.86;
+    echo $float_number;
+    echo 5 + 7;
+    $last_month = 1187.23;
+    $this_month = 1089.98;
+    echo $last_month - $this_month;
 
 ?>
