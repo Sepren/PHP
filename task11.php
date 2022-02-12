1602fecad8f449514af738537ae8e52666990dbf
Sepren <LexusLFA477@gmail.com>
2022-02-13 01:44:06 +0200

    Create and made a new task

---
+++ task11.php
@@ -0,0 +1,8 @@
+<?php
+    $num_languages = 4;
+    $months = 11;
+    $days = $months * 16 / 4;
+    $days_per_language = $days;
+    echo "В среднем, ушло на изучение каждого языка", $days_per_language;
+
+?>
