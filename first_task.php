96bd402270f21c2f28156c5f4ccf62dc5ab9096f
Sepren <LexusLFA477@gmail.com>
2022-02-13 01:26:50 +0200

    Create and made the first php task

--- first_task.php
+++ first_task.php
@@ -1,8 +1,8 @@
 <?php
 /* Imagine a lot of code here */
-$very_bad_unclear_name = "15 chicken wings";
-$order = & $very_bad_unclear_name;
-$order .= "kuki";
-// Don't change the line below
-echo "\nYour order is: $very_bad_unclear_name.";
+    $number = 15;
+    echo $number;
+    $new_number = 10;
+    echo $new_number, "\n";
+
 ?>
