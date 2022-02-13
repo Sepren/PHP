8cf16f2aeacebb07a2996e0c6fd7645f073d231f
Sepren <LexusLFA477@gmail.com>
2022-02-13 18:23:57 +0200

    upgrade a file

--- coding/task_16.php
+++ coding/task_16.php
@@ -1,18 +1,58 @@
 <?php
+
     function increaseEnthusiasm($string)
     {
         echo $string, "!";
+        echo "\n";
     }
-    $str = readline();
-    increaseEnthusiasm($str);
 
-    function repeatThreeTimes($string)
+    function repeatThreeTimes($repeat)
     {
-        echo $string * 3;
+        $items = strval($repeat);
+        increaseEnthusiasm($items * 3);
     }
+
     $str = readline();
     repeatThreeTimes($str);
 
+    function cut($our_str, $save_str)
+    {
+        $result = "";
+        for ($i = 0; $i < 10; $i ++)
+        {
+            $result .= $our_str[$i];
+        }
+        return $result;
+    }
+    $our_string = readline();
+    $save_string = 10;
+    echo cut($our_string, $save_string);
+    echo "\n";
 
+    function our_quantity(array $quantity, $count)
+    {
+        if ($count < sizeof($quantity))
+        {
+            echo $quantity[$count], " ";
+            our_quantity($quantity, $count + 1);
+        }
+    }
+    $array = [7, 1, 5, 4, 5, 6];
+    our_quantity($array, 0);
+    echo "\n";
+
+    function digit_9($digit)
+    {
+        $sum = 0;
+        while ($digit > 0)
+        {
+            $sum += $digit % 10;
+            $digit /= 10;
+        }
+        if ($sum >= 10) return digit_9($sum);
+        else return $sum;
+    }
+    $number = readline();
+    echo digit_9($number);
 
 ?>
