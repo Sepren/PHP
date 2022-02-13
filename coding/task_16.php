18c8b1700cf1714d1f53792870b0dfd79765b82f
Sepren <LexusLFA477@gmail.com>
2022-02-13 17:27:55 +0200

    Create a new file

---
+++ coding/task_16.php
@@ -0,0 +1,18 @@
+<?php
+    function increaseEnthusiasm($string)
+    {
+        echo $string, "!";
+    }
+    $str = readline();
+    increaseEnthusiasm($str);
+
+    function repeatThreeTimes($string)
+    {
+        echo $string * 3;
+    }
+    $str = readline();
+    repeatThreeTimes($str);
+
+
+
+?>
