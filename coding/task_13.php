66bb45c174c6e7c128cf1fd50338bb117f76afd8
Sepren <LexusLFA477@gmail.com>
2022-02-13 15:18:40 +0200

    Create a new file

---
+++ coding/task_13.php
@@ -0,0 +1,11 @@
+<?php
+    $my_num = 1;
+    $answer = $my_num;
+    $answer += 2;
+    $answer *= 2;
+    $answer -= 2;
+    $answer /= 2;
+    $answer = $answer - $my_num;
+    echo $answer;
+
+?>
